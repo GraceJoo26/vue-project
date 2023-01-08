d5757fd06ceec1f52994258fe572f4bc6a77b33a
gracejoo26 <gracejoo26@gmail.com>
2023-01-08 10:15:28 +0900

    button click event

--- src/views/Event1.vue
+++ src/views/Event1.vue
@@ -1,3 +1,13 @@
 <template>
-    <div>hi</div>
+    <button @click="counter++">누르면1추가</button>
+    <p>이 버튼은 {{ counter }}회 눌렸습니다.</p>
 </template>
+<script>
+    export default{
+        data() {
+            return{
+                counter:0
+            }
+        }
+    }
+</script>
