a28b48783f023335cc6e4580f7b6c7640b27bc43
gracejoo26 <gracejoo26@gmail.com>
2023-01-08 12:20:25 +0900

    mouseMoving event

---
+++ src/views/Event5.vue
@@ -0,0 +1,14 @@
+<template>
+    <MouseTracker v-slot="{x,y}">
+    마우스는 지금: {{ x }},{{ y }} 에 있습니다.
+    </MouseTracker>
+</template>
+<script>
+import MouseTracker from './MouseTracker.vue'
+
+export default{
+    components:{
+        MouseTracker
+    }
+}
+</script>
