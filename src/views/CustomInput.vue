f14be0243ee5e6e7b94379402ea7ac971ab4c810
gracejoo26 <gracejoo26@gmail.com>
2023-01-08 11:51:50 +0900

    customInput v-model binding

---
+++ src/views/CustomInput.vue
@@ -0,0 +1,12 @@
+<template>
+    <input
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"/>
+    <!-- emit 이란? 상위이벤트->하위이벤트로 이벤트 전달때 사용함-->
+</template>
+<script>
+export default {
+    props:['modelValue'],
+    emits:['update:modelValue']
+}
+</script>
---
+++ src/views/Event4.vue
@@ -0,0 +1,15 @@
+<template>
+   <CustomInput v-model="message" placeholder="입력해보세요."/>
+   {{ message }}
+</template>
+<script>
+import CustomInput from './CustomInput.vue'
+export default{
+    components: { CustomInput },
+    data() {
+        return {
+            message:'주유진'
+        }
+    }
+}
+</script>
