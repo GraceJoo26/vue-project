6877b831cfc5bdcad214ac53021f89140d4b7522
gracejoo26 <gracejoo26@gmail.com>
2023-01-08 12:03:54 +0900

    주석처리

--- src/views/CustomInput.vue
+++ src/views/CustomInput.vue
@@ -9,4 +9,6 @@ export default {
     props:['modelValue'],
     emits:['update:modelValue']
 }
+//1. native input에서 modelValue porperty로 연동시켜야함
+//2. native input 이벤트가 생기면 update:modelValue 커스텀 이벤트를 새로운 value에 emit(연동)해야함
 </script>
